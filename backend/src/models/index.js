d657bf92daae2a8ac4917d2ee05642beab5b7dae
Omarcito007 <brayancordovaaguilar1@gmail.com>
2026-04-07 20:51:14 -0500

    Actualizando componentes del gestor de tareas

--- backend/src/models/index.js
+++
@@ -1,9 +0,0 @@
-const User = require('./models/users');
-const Task = require('./models/task');
-
-//un usuario tiene muchas tareas
-User.hasMany(Task);
-//una tarea pertenece a un usuario
-Task.belongsTo(User);
-
-GPUShaderModule.exports = {User,Task};
